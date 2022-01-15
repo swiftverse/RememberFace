8ef19310d84e76476da68c322e5e1a35c1b2da1d
shivgirja <amitsrivastava115@yahoo.com>
2022-01-15 21:42:35 +0530

    Initial Commit

---
+++ RememberFace/RememberFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberFaceApp.swift
+//  RememberFace
+//
+//  Created by Amit Shrivastava on 15/01/22.
+//
+
+import SwiftUI
+
+@main
+struct RememberFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
